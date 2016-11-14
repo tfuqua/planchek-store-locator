2f389393d55cb72fd5b65160d8c8d35d60077b9f
Taylor Fuqua <tfuqua@gwu.edu>
2016-11-14 17:36:13 -0500

    Move JS to external JS file, only load JS on needed pages

---
+++ lib/store-locator.php
@@ -0,0 +1,156 @@
+<?php
+/*
+  Plugin Name: Ukrops Store Locator
+  Version: 1.0
+  Author: Taylor Fuqua
+*/
+
+?>
+<?php
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<!-- Hero Image -->
+	    <?php if(get_field('hero_image')) { ?>
+	      <?php $background = wp_get_attachment_image_src(get_field('hero_image'), 'full', false); ?>
+		     <div class="hero store-hero" style="background-image: url('<?php echo $background[0] ?>');">
+	         <div class="hero-text-wrapper">
+	           <div>
+	             <div class="hero-text">
+								 <?php
+								 		while ( have_posts() ) : the_post();
+											the_title();
+					 					endwhile; // End of the loop.
+									?>
+	             </div>
+	           </div>
+	         </div>
+	      </div>
+	    <?php } ?>
+
+		<div class="container-fluid">
+
+      <!-- Store Locator Map -->
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-3 search-column">
+              <form class="store-locator" onsubmit="return newMap()">
+	              <h3>Store Locator</h3>
+                <div class="form-group">
+                  <label>Zip Code</label>
+                  <input id="zip-val" value="<?php echo $zip; ?>" type="text" class="form-control" name="zip" />
+                </div>
+
+                <div class="form-group">
+                  <label>Brand</label>
+                  <select id="brand-val" class="form-control" name="brand">
+                    <option value="all">All Brands</option>
+										<?php foreach($brandList as $brands) { ?>
+											<option value="<?php echo $brands->brand ?>" <?php if ($brands->brand == $brand){echo 'selected';}?>>
+												<?php echo $brands->brand ?>
+											</option>
+										<?php }?>
+                  </select>
+                </div>
+
+                <div class="form-group">
+                  <input type="submit" value="Search" class="btn search"/>
+                </div>
+              </form>
+
+
+          </div> <!-- .col-3 -->
+          <div class="col-md-9 map-column">
+						<?php if ($firstVisit) {
+				      while ( have_posts() ) : the_post(); ?>
+								<div class="col-lg-10 col-md-10 store-intro">
+									<?php the_content();?>
+								</div>
+								<div class="clearfix">	</div>
+							<?php
+						endwhile; // End of the loop.?>
+
+						<div class="store-branding">
+							<?php if( have_rows('brands') ) {
+									while ( have_rows('brands') ) : the_row(); ?>
+										<h3><?php echo get_sub_field('brand'); ?></h3>
+										<div class="store-imgs">
+												<?php while ( have_rows('images') ) : the_row(); ?>
+													<div class="img-wrapper">
+														<?php echo wp_get_attachment_image(get_sub_field('image'), 'full', false, array( 'class' => ''));?>
+													</div>
+												<?php endwhile;?>
+										</div>
+									<?php
+									endwhile;
+								} ?>
+						</div>
+							<?php
+							}	else {
+							?>
+            	<div id="map" style="width:100%; height:400px;"></div>
+						<?php
+						} ?>
+          </div>	<!-- col-9 -->
+        </div> <!-- row -->
+
+				<?php if (!$firstVisit) { ?>
+				<div class="row">
+					<div class="col-xs-12 hidden-xs">
+						<br />
+						<table id="store-table" class="table store-locator"></table>
+					</div>
+				</div> <!-- row -->
+				<?php
+				} ?>
+
+      </div> <!-- container-fluid -->
+		</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+	<?php
+		$i = 1;
+		$storeSize = count($data);
+		echo '<div id="store-data" style="display:none;">[';
+		foreach ($data as $store) {
+			$storeJSON = 	'{'.
+										'"address": "' 	. $store->address . '", '.
+										'"name": "' 		. $store->store_name . '", '.
+										'"brand": "' 		. $store->brand . '", '.
+										'"phone": "' 		. $store->phone . '", '.
+										'"city": "' 		. $store->city . '", '.
+										'"state": "' 		. $store->state . '", '.
+										'"products": "' . $store->products . '", '.
+										'"latitude":' 	. $store->latitude . ', '.
+										'"longitude":' 	.	$store->longitude .
+										' } ';
+
+			echo $storeJSON;
+
+			if ($i < $storeSize){
+				echo ', ';
+			}
+			$i++;
+		}
+		echo ']</div>';?>
+	<br />
+	<?php if (!$firstVisit) { ?>
+		<script>
+			var storeData = JSON.parse($("#store-data").html());
+			var table; //$('#store-table').DataTable();
+			var markers = [];
+			var map;
+			var zoom = <?php echo $zoom; ?>;
+			var zip = <?php echo $zip ?>;
+			var brand = '<?php echo $brand ?>';
+			var infowindow;
+		</script>
+	<?php
+		} else { ?>
+		<script> function newMap() { return true; } </script>
+	<?php
+		}
+  get_footer();  ?>
--- ukrops-store-locator.php
+++ ukrops-store-locator.php
@@ -8,13 +8,10 @@ Author: Taylor Fuqua
 class Ukrops_Store_Locator {
 
 
-	/* A reference to an instance of this class. */
-	private static $instance;
 
-	/* The array of templates that this plugin tracks. */
+	private static $instance;
 	protected $templates;
 
-	/* Returns an instance of this class. */
 	public static function get_instance() {
 
 		if ( null == self::$instance ) {
@@ -24,73 +21,49 @@ class Ukrops_Store_Locator {
 		return self::$instance;
 	}
 
-	/* Initializes the plugin by setting filters and administration functions.*/
+
 	private function __construct() {
 
 		$this->templates = array();
 
-		// Add a filter to the attributes metabox to inject template into the cache.
 		add_filter('page_attributes_dropdown_pages_args', array( $this, 'register_project_templates' ));
-
-		// Add a filter to the save post to inject out template into the page cache
 		add_filter('wp_insert_post_data',array( $this, 'register_project_templates' ));
-
-		// Add a filter to the template include to determine if the page has our
-		// template assigned and return it's path
 		add_filter('template_include', array( $this, 'view_project_template'));
 
-		// Add your templates to this array.
-		$this->templates = array('lib/template.php' => 'Store Locator',);
+		$this->templates = array('lib/store-locator.php' => 'Store Locator',);
 	}
 
 	public function register_project_templates( $atts ) {
 
-		// Create the key used for the themes cache
 		$cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
 
-		// Retrieve the cache list If it doesn't exist, or it's empty prepare an array
 		$templates = wp_get_theme()->get_page_templates();
 		if ( empty( $templates ) ) {
 			$templates = array();
 		}
 
-		// New cache, therefore remove the old one
 		wp_cache_delete( $cache_key , 'themes');
-
-		// Now add our template to the list of templates by merging our templates
-		// with the existing templates array from the cache.
 		$templates = array_merge( $templates, $this->templates );
-
-		// Add the modified cache to allow WordPress to pick it up for listing
-		// available templates
 		wp_cache_add( $cache_key, $templates, 'themes', 1800 );
 
 		return $atts;
-
 	}
 
-	/**
-	 * Checks if the template is assigned to the page
-	 */
 	public function view_project_template( $template ) {
 
 
-		// Get global post
 		global $post;
 
-		// Return template if post is empty
-		if ( ! $post ) {
+		if ( !$post ) {
 			return $template;
 		}
 
-		// Return default template if we don't have a custom one defined
 		if ( !isset( $this->templates[get_post_meta($post->ID, '_wp_page_template', true)] ) ) {
 			return $template;
 		}
 
 		$file = plugin_dir_path(__FILE__). get_post_meta($post->ID, '_wp_page_template', true);
 
-		// Just to be safe, we check if the file exist first
 		if ( file_exists( $file ) ) {
 
 			$data = self::getStores();
@@ -140,8 +113,7 @@ class Ukrops_Store_Locator {
 	}
 
 	public function add_menu_item() {
-		add_menu_page('Page title', 'Stores', 'manage_options', 'stores',
-		array( 'Ukrops_Store_Locator', 'store_locator_form'));
+		add_menu_page('Page title', 'Store Locator', 'manage_options', 'stores', array( 'Ukrops_Store_Locator', 'store_locator_form'));
 	}
 
 	public function handle_post(){
@@ -266,7 +238,6 @@ class Ukrops_Store_Locator {
     $table_name = $wpdb->prefix . "stores";
 		$charset_collate = $wpdb->get_charset_collate();
 
-		//if( $wpdb->get_var( "show tables like '{$table_name}'" ) != $table_name ) {
 			$sql = "CREATE TABLE $table_name (
 			  id mediumint(9) NOT NULL AUTO_INCREMENT,
 			  brand varchar(55) NOT NULL,
@@ -286,6 +257,20 @@ class Ukrops_Store_Locator {
 			dbDelta( $sql );
 	}
 }
+
+function store_scripts() {
+
+	//Only load scripts on page that needs them
+	if (get_page_template_slug() == 'lib/store-locator.php') {
+		wp_enqueue_script( 'datatables', 'https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js' , array('ukrops-jquery'), '1.0', true);
+		wp_enqueue_script( 'google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDV9ffngNmBtR8tC_9g37OL7QZhEheyxQw&libraries=geometry', array(), '1.0', true);
+		wp_enqueue_script( 'store-js', plugin_dir_url( __FILE__ ) . 'lib/js/store-locator.js', array('google-maps'), '1.0', true);
+	} else {
+
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'store_scripts' );
 
 add_action('init', array( 'Ukrops_Store_Locator', 'init'));
 add_action( 'plugins_loaded', array( 'Ukrops_Store_Locator', 'get_instance' ) )
